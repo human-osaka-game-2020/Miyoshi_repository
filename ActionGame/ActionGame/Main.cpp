8795b9b54cca93b81fcdd7bf87991484c143239a
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2020-10-16 14:10:59 +0900

    フェードの実装 - #1
    ファンクションヘッダの作成
    ・ゲームの機能に関する処理を記述
    
    <Function>
    フェードイン : FadeIn(..)
    ・フェードの速さ
    ・フェード前の色
    ・フェード前の待ち時間
    
    フェードアウト : FadeOut(..)
    ・フェードの速さ
    ・フェード後の色
    ・フェード後の待ち時間
    
    フェードの処理部分 : Fade(..)
    ・フェードの種類
    ・フェードの速さ
    ・フェード前/後の色
    ・フェード前/後の待ち時間
    
    <Common>
    定数を追加
    ・色
    ・ウィンドウサイズ

--- ActionGame/ActionGame/Main.cpp
+++ ActionGame/ActionGame/Main.cpp
@@ -1,56 +1,75 @@
 #include "Script/Header/Common.h"
 
 // ゲーム処理
-void GameProcessing();
+void GameProcessing ();
 // 描画処理
-void DrawProcessing();
+void DrawProcessing ();
 
 Player player;
 
+FadeMode fadeMode = Mode_FadeNone;
+
 // プログラムは WinMain から始まります
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
 {
+
 	SetGraphMode ( WINDOW_WIDTH, WINDOW_HEIGHT, 32, 60 );
 	ChangeWindowMode ( true ); // ウィンドウモードに変更
 
-	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
+	if ( DxLib_Init () == -1 )	// ＤＸライブラリ初期化処理
 	{
 		return -1; // エラーが起きたら直ちに終了
 	}
 
 	// グラフィックの描画先を裏画面にセット
-	SetDrawScreen(DX_SCREEN_BACK);
+	SetDrawScreen ( DX_SCREEN_BACK );
 
 	// メインループ
-	while (true)
+	while ( true )
 	{
-		GameProcessing();
-		DrawProcessing();
+		// 画面を初期化
+		ClearDrawScreen ();
+
+		GameProcessing ();
+		DrawProcessing ();
 
 		// -1 が返ってきたらループを抜ける
-		if (ProcessMessage() < 0) break;
+		if ( ProcessMessage () < 0 ) break;
 		// もしＥＳＣキーが押されていたらループから抜ける
-		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
+		if ( CheckHitKey ( KEY_INPUT_ESCAPE ) ) break;
+
+		// 裏画面の内容を表画面にコピーする
+		ScreenFlip ();
 	}
 
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
+	DxLib_End ();				// ＤＸライブラリ使用の終了処理
 
 	return 0;					// ソフトの終了
 }
 
-void GameProcessing() {
+void GameProcessing () {
 	player.Moving();
+
+	if ( fadeMode == Mode_FadeNone ) {
+		if ( CheckHitKey( KEY_INPUT_SPACE ) ) {
+			fadeMode = Mode_FadeIn;
+		}
+		else if ( CheckHitKey( KEY_INPUT_RETURN ) ) {
+			fadeMode = Mode_FadeOut;
+		}
+	}
 }
 
 void DrawProcessing() {
+	player.Draw();
 
 	if ( fadeMode == Mode_FadeIn ) {
-		if ( FadeIn ( 255 / 60, COLOR_BLUE ) ) {
+		if ( FadeIn( 255 / 60, COLOR_BLUE, 30 ) ) {
 			fadeMode = Mode_FadeNone;
 		}
 	}
 	else if ( fadeMode == Mode_FadeOut ) {
-		if ( FadeOut ( 255 / 60, COLOR_RED, 90 ) ) {
+		if ( FadeOut( 255 / 60, COLOR_RED, 90 ) ) {
 			fadeMode = Mode_FadeNone;
 		}
 	}
--- ActionGame/ActionGame/Script/Header/Function.h
+++ ActionGame/ActionGame/Script/Header/Function.h
@@ -1,51 +1,38 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+enum FadeMode {
+	Mode_FadeNone,
+	Mode_FadeIn,
+	Mode_FadeOut
+};
+
 /// <summary>
-/// フェードイン
+/// フェード処理の中身
 /// </summary>
-<<<<<<< HEAD
-/// <param name="fadePower">フェードインの速さ</param>
-/// <param name="fadeColor">フェードの色 GetColor関数を推奨</param>
-void FadeIn(float fadePower, int fadeColor = GetColor(0, 0, 0));
-=======
 /// <param name="fademode">フェードの種類</param>
 /// <param name="fadePower">フェードの速さ</param>
 /// <param name="fadeColor">フェードする色</param>
 /// <param name="waitTime">フェードの待ち時間(フレーム)</param>
 /// <returns>true == 処理終了, false == 処理中</returns>
-bool Fade(FadeMode fademode ,unsigned int fadePower, int fadeColor, int waitTime );
->>>>>>> 69c635c... 繝ｪ繝輔ぃ繧ｯ繧ｿ繝ｪ繝ｳ繧ｰ
+bool Fade( FadeMode fademode, unsigned int fadePower, int fadeColor, int waitTime );
 
 /// <summary>
-/// フェードアウト
+/// フェードイン
 /// </summary>
-<<<<<<< HEAD
-/// <param name="fadePower">フェードアウトの速さ</param>
-/// <param name="fadeColor">フェードの色 GetColor関数を推奨</param>
-void FadeOut(float fadePower, int fadeColor = GetColor(0, 0, 0));
-=======
 /// <param name="fadePower">フェードの速さ</param>
 /// <param name="fadeColor">フェードインする前の色</param>
 /// <param name="waitTime">フェードが始まる前に待つ時間(フレーム)</param>
 /// <returns>true == 処理終了, false == 処理中</returns>
-bool FadeIn(unsigned int fadePower, int fadeColor = GetColor(0, 0, 0), int waitTime = 0 );
->>>>>>> 69c635c... 繝ｪ繝輔ぃ繧ｯ繧ｿ繝ｪ繝ｳ繧ｰ
+bool FadeIn( unsigned int fadePower, int fadeColor = GetColor( 0, 0, 0 ), int waitTime = 0 );
 
 /// <summary>
-/// カラーフェード
+/// フェードアウト
 /// </summary>
-<<<<<<< HEAD
-/// <param name="fadePower">色の切り替わる速さ</param>
-/// <param name="startColor">元の色 GetColor関数を推奨</param>
-/// <param name="endColor">目標の色 GetColor関数を推奨</param>
-void ColorFade(float fadePower, int startColor, int endColor);
-=======
 /// <param name="fadePower">フェードの速さ</param>
 /// <param name="fadeColor">フェードアウトした後の色</param>
 /// <param name="waitTime">フェードが終わった後に待つ時間(フレーム)</param>
 /// <returns>true == 処理終了, false == 処理中</returns>
-bool FadeOut(unsigned int fadePower, int fadeColor = GetColor(0, 0, 0), int waitTime = 0 );
->>>>>>> 69c635c... 繝ｪ繝輔ぃ繧ｯ繧ｿ繝ｪ繝ｳ繧ｰ
+bool FadeOut( unsigned int fadePower, int fadeColor = GetColor( 0, 0, 0 ), int waitTime = 0 );
 
 #endif // !FUNCTION_H
