95ac2076fedffdc81d6f89745efc0d3ee50370ad
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2020-10-16 14:30:13 +0900

    入力状態の取得 - #5
    - 入力状態を更新する -
    ・CheckKeyPushed( bool pStateArray )
    現在キーが押されているかどうかを全てのキーでチェックして、bool型の配列に保存する。
    
    ・UpdateKeyState()
    現在の入力状態と、１フレーム前の入力状態をもとに、キーの入力状態を更新する。
    
    ・UpdateMouseButtonState()
    現在の入力状態と、１フレーム前の入力状態をもとに、マウスボタンの入力状態を更新する。
    
    - 入力状態を取得する -
    ・GetKeyState( int keyCode )
    キーの入力状態をInputState型で返す。
    
    ・GetMouseButtonState( int mouseButtonCode )
    マウスボタンの入力状態をInputState型で返す。
    
    - 入力状態を無効にする -
    SwitchEnabledKeyInput( int keyCode )
    ・指定したキーの無効/有効を切り替える。
    
    SwitchEnabledMouseButtonInput( int keyCode )
    ・指定したマウスボタンの無効/有効を切り替える。

--- ActionGame/ActionGame/Script/Function.cpp
+++ ActionGame/ActionGame/Script/Function.cpp
@@ -1,6 +1,12 @@
 ﻿
 #include "Header/Common.h"
 
+// キーが何フレーム入力されているか保存する
+InputState keyState[256];
+
+// マウスボタンが何フレーム入力されているか保存する
+InputState mouseState[MOUSEBUTTON_UPDATE_RANGE] = {};
+
 bool Fade( FadeMode fademode, unsigned int fadePower, int fadeColor, int waitTime ) {
 
 	const int ALPHA_MAX = 255;
@@ -51,4 +57,77 @@ bool FadeIn(unsigned int fadePower, int fadeColor, int waitTime ) {
 
 bool FadeOut(unsigned int fadePower, int fadeColor, int waitTime ) {
 	return Fade ( FadeMode::Out, fadePower, fadeColor, waitTime ) ? true : false;
+}
+
+void CheckKeyPushed( bool* pStateArray ){
+	char temp[256];
+
+	GetHitKeyStateAll( temp );
+
+	for( int i = 0; i < 256; i++ ){
+		pStateArray[i] = ( temp[i] == 1 ) ? true : false;
+	}
+}
+
+/// @brief キー/マウスの入力状態を更新する
+/// @param isPressed 現在キーが押されているか
+/// @param inputState キー/マウスの入力状態
+/// @return 入力状態の判定結果 InputStateで返す
+InputState UpdateInputState( bool isPressed, InputState inputState ){
+	if ( inputState == InputState::Invalid )return InputState::Invalid;
+
+	if ( isPressed == true ){
+		if ( inputState == InputState::NotPressed ){
+			return InputState::Pressed;
+		}
+		else if ( inputState == InputState::Pressed || inputState == InputState::Pressing ){
+			return InputState::Pressing;
+		}
+	}
+	else{
+		if ( inputState == InputState::Pressed || inputState == InputState::Pressing ){
+			return InputState::Released;
+		}
+	}
+
+	return InputState::NotPressed;
+}
+
+int UpdateKeyState() {
+	bool currentKeyState[256];
+
+	CheckKeyPushed( currentKeyState );
+
+	for ( int i = 0; i < 256; i++ ) {
+		keyState[i] = UpdateInputState( currentKeyState[i], keyState[i] );
+	}
+
+	return 0;
+}
+
+InputState GetKeyStatus( int keyCode ){
+	return keyState[keyCode];
+}
+
+void SwitchEnabledKeyInput( int keyCode ) {
+	keyState[keyCode] = ( keyState[keyCode] == InputState::Invalid ) ? InputState::NotPressed : InputState::Invalid;
+}
+
+int UpdateMouseButtonState() {
+	bool currentMouseState[MOUSEBUTTON_UPDATE_RANGE];
+
+	for ( int i = 0; i < MOUSEBUTTON_UPDATE_RANGE; i++ ) {
+		currentMouseState[i] = ( ( GetMouseInput() & i ) != 0 ) ? true : false;
+		mouseState[i] = UpdateInputState( currentMouseState[i], mouseState[i] );
+	}
+
+	return 0;
+}
+
+InputState GetMouseButtonStatus( int mouseButtonCode ) {
+	return mouseState[mouseButtonCode];
+}
+
+void SwitchEnabledMouseButtonInput( int mouseButtonCode ) {
+	mouseState[mouseButtonCode] = ( mouseState[mouseButtonCode] == InputState::Invalid ) ? InputState::NotPressed : InputState::Invalid;
 }
