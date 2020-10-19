568fa38cce5eba698b38296273e7f8bc139aa882
taishi-matsumura <taishi0706@gmail.com>
2020-10-19 15:55:33 +0900

    フェード関数を整理

--- ActionGame/ActionGame/Script/Header/Function.cpp
+++ ActionGame/ActionGame/Script/Header/Function.cpp
@@ -1,44 +1,30 @@
-#include "Common.h"
+﻿#include "Common.h"
 
-bool Fade(FadeMode fademode, unsigned int fadePower, int waitTime, int fadeColor) {
-	
-	static int alpha = 0;
-	static bool endFade = false;
+bool Fade(FadeMode fadeMode, unsigned int fadePower, int waitTime, int fadeColor) {
+	const int ALPHA_MIN = 0x00;
+	const int ALPHA_MAX = 0xFF;
+	const int ALPHA_INVALID = -1;
 
-	if (fademode == FadeMode::FadeOut) {
-		fadePower *= -1;
-		if (alpha == 0) {
-			alpha = 256;
-		}
+	static int alpha = ALPHA_INVALID;	// 初期値は無効な値にしておく
+
+	if (alpha == ALPHA_INVALID) {
+		// ここに入るのは、フェード処理開始フレームのみ
+		alpha = (fadeMode == FadeMode::FadeIn) ? ALPHA_MAX : ALPHA_MIN;
 	}
 
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, alpha);
-	if (fadeColor != 0) {
-		DrawBox(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, fadeColor, true);
-	}
-	alpha += (fadePower);
+	DrawBox(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, fadeColor, true);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	if (fademode == FadeMode::FadeIn) {
-		if (alpha > 256) {
-			alpha = 0;
-			endFade = true;
-		}
-	}
-	else if (fademode == FadeMode::FadeOut) {
-		if (alpha < 1) {
-			alpha = 256;
-			endFade = true;
-		}
+	if (fadeMode == FadeMode::FadeIn) {
+		fadePower *= -1;
 	}
 
-	if (endFade == true) {
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-
-		WaitTimer(waitTime);
-		endFade = false;
+	alpha += fadePower;
+	if (alpha < ALPHA_MIN || ALPHA_MAX < alpha) {
+		alpha = ALPHA_INVALID;
 		return true;
 	}
 
 	return false;
-
 }
